64f4482ccc751f73e5a36fbd9203b2020c71a91c
michaeldeistler <michael.deistler@uni-tuebingen.de>
2023-01-16 13:26:08 +0100

    folder structure

---
+++ gbi/benchmark/generate_gt/run.py
@@ -0,0 +1,51 @@
+import pickle
+import os
+import torch
+import numpy as np
+import time
+
+import hydra
+from omegaconf import DictConfig
+from hydra.utils import get_original_cwd, to_absolute_path
+
+import logging
+
+from gbi.tasks.linear_gaussian.task import LinearGaussian
+from gbi.generate_gt.mcmc import run_mcmc
+from gbi.generate_gt.flow import train_flow
+from gbi.generate_gt.rejection import run_rejection
+
+log = logging.getLogger("run_benchmark_gt")
+
+
+@hydra.main(version_base="1.1", config_path="config", config_name="run")
+def run(cfg: DictConfig) -> None:
+
+    dir_path = get_original_cwd()
+
+    with open(f"{dir_path}/../tasks/{cfg.task.name}/xo.pkl", "rb") as handle:
+        simulated_x = pickle.load(handle)
+    x_o = simulated_x[cfg.task.xo_index].unsqueeze(0)
+
+    # Define task.
+    if cfg.task.name == "linear_gaussian":
+        task = LinearGaussian(x_o=x_o, beta=cfg.task.beta)
+    else:
+        raise NameError
+
+    if cfg.seed is None:
+        seed = int((time.time() % 1) * 1e7)
+    else:
+        seed = cfg.seed
+    np.savetxt("seed.txt", seed)
+
+    # Run ground truth suite.
+    _ = torch.manual_seed(seed)
+    _ = np.random.seed(seed=seed)
+    run_mcmc(task)
+    train_flow(**cfg.flow)
+    run_rejection(task)
+
+
+if __name__ == "__main__":
+    run()
