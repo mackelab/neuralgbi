8ca62bcc9f070efd10d5ae0dc83bb3a5c42c1be4
michaeldeistler <michael.deistler@uni-tuebingen.de>
2023-01-17 14:45:12 +0100

    typing

--- gbi/benchmark/tasks/uniform_1d/task.py
+++ gbi/benchmark/tasks/uniform_1d/task.py
@@ -1,4 +1,4 @@
-from typing import Tuple, Optional
+from typing import Tuple, Optional, Union
 
 import torch
 from torch import tensor, as_tensor, float32, ones, zeros, eye, randn, Tensor
@@ -11,59 +11,64 @@ class UniformNoise1D:
         prior_bounds: Tuple = (-1.5, 1.5),
         beta: float = 1.0,
         seed: int = 0,
-        poly_coeffs: Tensor = Tensor([ 0.1627,  0.9073, -1.2197, -1.4639,  1.4381]),
-        epsilon: Tensor = 0.25,
+        poly_coeffs: Tensor = Tensor([0.1627, 0.9073, -1.2197, -1.4639, 1.4381]),
+        epsilon: Union[Tensor, float] = 0.25,
         x_o: Optional[Tensor] = None,
     ):
         # Set seed.
         _ = torch.manual_seed(seed)
-        
+
         # Make prior for theta.
-        self.prior = BoxUniform(prior_bounds[0]*ones(1,), prior_bounds[1]*ones(1,))
-        
+        self.prior = BoxUniform(prior_bounds[0] * ones(1), prior_bounds[1] * ones(1))
+
         # noise_likelihood model.
-        self.noise_likelihood = BoxUniform(-epsilon*ones(1,), epsilon*ones(1,))
-        
-        # Set polynomial coefficients; default makes good curve.        
-        self.poly_coeffs = poly_coeffs            
-        
+        self.noise_likelihood = BoxUniform(-epsilon * ones(1), epsilon * ones(1))
+
+        # Set polynomial coefficients; default makes good curve.
+        self.poly_coeffs = poly_coeffs
+
         # Set uniform noise half-width.
         self.epsilon = epsilon
 
         # Set GBI likelihood temperature.
         self.beta = beta
-        
+
         # Set observation
         self.x_o = x_o
-        
+
     def simulate_noiseless(self, theta: Tensor) -> Tensor:
         """Noiseless simulator."""
-        return (torch.hstack([(.8*(theta+0.25))**i for i in range(5)]) * self.poly_coeffs).sum(1, keepdim=True)
-    
+        return (
+            torch.hstack([(0.8 * (theta + 0.25)) ** i for i in range(5)])
+            * self.poly_coeffs
+        ).sum(1, keepdim=True)
+
     def simulate(self, theta: Tensor) -> Tensor:
         """Simulator with U[-eps, eps] noise applied."""
         # Get uniform noise of [-epsilon, epsilon].
-        noise = self.noise_likelihood.sample((theta.shape[0],))
+        noise = self.noise_likelihood.sample((theta.shape[0],))  # type: ignore
         return self.simulate_noiseless(theta) + noise
-        
+
     def distance_fn(self, theta):
         """Computes E_{x|t}[(x - x_o)^2]."""
         x_true = self.simulate_noiseless(theta)
-        x_low =  x_true - self.epsilon
-        x_high =  x_true + self.epsilon
-        expected_value_of_d = self._eval_distance_integral(x_high) - self._eval_distance_integral(x_low)
+        x_low = x_true - self.epsilon
+        x_high = x_true + self.epsilon
+        expected_value_of_d = self._eval_distance_integral(
+            x_high
+        ) - self._eval_distance_integral(x_low)
         return expected_value_of_d
-    
+
     def potential(self, theta):
         """Potential for GBI ground truth posterior."""
         term1 = -self.beta * self.distance_fn(theta)
         return term1 + self.prior.log_prob(theta)
-    
-    
+
     def _eval_distance_integral(self, x):
         """Integral of (x-x_o)^2 (without constant)."""
-        return (x**3)/3 - self.x_o*x**2 + x*self.x_o**2
-        
+        assert self.x_o is not None
+        return (x**3) / 3 - self.x_o * x**2 + x * self.x_o**2
+
     # def true_posterior_linear_gaussian(self, x_o: Tensor) -> MultivariateNormal:
     #     """Returns the Bayes posterior."""
 
