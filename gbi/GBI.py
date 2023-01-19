45b118314a080e5375fa7eb454a17428ab0dbe30
rdgao-lajolla <r.dg.gao@gmail.com>
2023-01-19 13:25:32 +0100

    doc for GBI.py

--- gbi/GBI.py
+++ gbi/GBI.py
@@ -1,8 +1,11 @@
 import matplotlib.pyplot as plt
+from typing import Any, Callable, Dict, Optional, Union
+
 import numpy as np
 import torch
-from torch import Tensor, nn, optim
+from torch import Tensor, nn, optim, functional
 from torch.utils.data import TensorDataset, DataLoader
+from torch.distributions import Distribution
 
 from sbi.utils.torchutils import atleast_2d
 from sbi.inference.potentials.base_potential import BasePotential
@@ -10,27 +13,39 @@ from sbi.inference.potentials.base_potential import BasePotential
 from pyknos.nflows.nn import nets
 
 
+
 class GBInference:
     def __init__(
-        self, prior, distance_func, do_precompute_distances=True, n_lowest=None
+        self,
+        prior: Distribution,
+        distance_func: Callable,
+        do_precompute_distances: bool = True,
+        n_lowest: Optional[int] = None,
     ):
         self.prior = prior
         self.distance_func = distance_func
         self.do_precompute_distances = do_precompute_distances
         self.n_lowest = n_lowest
 
-    def append_simulations(self, theta, x, x_target):
+    def append_simulations(self, theta: Tensor, x: Tensor, x_target: Tensor):
+        """Append simulation data: theta, x, and target x."""
         self.theta = theta
         self.x = x
         self.x_target = x_target
         if self.do_precompute_distances:
+            # Pre compute the distance function between all x and x_targets.
             self._precompute_distance()
             self._compute_index_pairs()
         return self
 
     def initialize_distance_estimator(
-        self, num_layers, num_hidden, net_type, net_kwargs={}
+        self,
+        num_layers: int,
+        num_hidden: int,
+        net_type: str = "resnet",
+        net_kwargs: Optional[Dict] = {},
     ):
+        """Initialize neural network for distance regression."""
         self.distance_net = DistanceEstimator(
             self.theta.shape[1],
             self.x.shape[1],
@@ -42,30 +57,31 @@ class GBInference:
 
     def train(
         self,
-        distance_net=None,
-        training_batch_size=100,
-        n_epochs=100,
-        validation_fraction=0.1,
-        print_every_n=10,
-        plot_losses=True,
+        distance_net: Optional[nn.Module] = None,
+        training_batch_size: int = 100,
+        n_epochs: int = 100,
+        validation_fraction: float = 0.1,
+        print_every_n: int = 10,
+        plot_losses: bool = True,
     ):
 
         ## TO DO:
         ## TAKE IS_CONVERGED FROM SBI
 
-        # Can use custome distance net, otherwise take existing in class.
+        # Can use custom distance net, otherwise take existing in class.
         if distance_net == None:
             distance_net = self.distance_net
         else:
             self.distance_net = distance_net
 
+        # Move the variables just for convenience.
         idx_train, theta, x_target, dist_precomputed = (
             self.idx_train,
             self.theta,
             self.x_target,
             self.distance_precomputed,
         )
-        # Define loss and optimizer
+        # Define loss and optimizer.
         nn_loss = nn.MSELoss()
         optimizer = optim.Adam(distance_net.parameters())
 
@@ -79,34 +95,37 @@ class GBInference:
         )
         dataloader = DataLoader(train_set, batch_size=training_batch_size, shuffle=True)
 
-        # Get validation set once
+        # Get validation set.
         theta_val, x_val, dist_val = self._idx_to_data(val_set[:])
 
+        # Training loop.
         train_losses, val_losses = [], []
         for e in range(n_epochs):
             for i_b, idx_batch in enumerate(dataloader):
                 optimizer.zero_grad()
 
-                # load batch of theta, x, and pre-computed distances
-                theta_batch, x_batch, dist_batch = self._idx_to_data(idx_batch)                
+                # Load batch of theta, x, and pre-computed distances.
+                theta_batch, x_batch, dist_batch = self._idx_to_data(idx_batch)
 
-                # forward pass for distances
+                # Forward pass for distances.
                 dist_pred = distance_net(theta_batch, x_batch).squeeze()
 
-                # training loss
+                # Training loss.
                 l = nn_loss(dist_batch, dist_pred)
                 l.backward()
                 optimizer.step()
                 train_losses.append(l.detach())
 
-            # compute validation loss
+            # Compute validation loss each epoch.
             dist_pred = distance_net(theta_val, x_val).squeeze()
             l_val = nn_loss(dist_val, dist_pred)
             val_losses.append([i_b * e, l_val.detach()])
 
+            # Print validation loss
             if e % print_every_n == 0:
-                print(f"{e}: {l}")
+                print(f"{e}: train loss: {l:.6f}, val loss: {l_val:.6f}")
 
+        # Plot loss curves for convenience.
         train_losses = torch.Tensor(train_losses)
         val_losses = torch.Tensor(val_losses)
         if plot_losses:
@@ -114,9 +133,10 @@ class GBInference:
         return distance_net
 
     def build_amortized_GLL(self, distance_net: nn.Module = None):
+        """Build generalized likelihood function from distance predictor."""
         if distance_net == None:
             distance_net = self.distance_net
-        # Build and return function
+        # Build and return function.
         def generalized_loglikelihood(theta: Tensor, x_o: Tensor):
             theta = atleast_2d(theta)
             dist_pred = distance_net(theta, x_o.repeat((theta.shape[0], 1))).squeeze(1)
@@ -125,15 +145,20 @@ class GBInference:
 
         return generalized_loglikelihood
 
-    def get_potential(self, x_o=None, beta=1.0):
+    def get_potential(self, x_o: Tensor = None, beta: float = 1.0):
+        """Make the potential function. Pass through call to GBIPotenial object."""
         return GBIPotential(self.prior, self.build_amortized_GLL(), x_o, beta)
 
-    def build_posterior(self, posterior_func, x_o=None, beta=1.0):
+    def build_posterior(
+        self, posterior_func: Callable, x_o: Tensor = None, beta: float = 1.0
+    ):
+        """Create posterior object using the defined potential function."""
         potential_func = self.get_potential(x_o, beta)
         posterior = posterior_func(potential_func, self.prior)
         return posterior
 
     def _precompute_distance(self):
+        """Pre-compute the distances of all pairs of x and x_target."""
         self.distance_precomputed = torch.hstack(
             [
                 self.distance_func(self.x.unsqueeze(1), x_t).unsqueeze(1)
@@ -142,6 +167,7 @@ class GBInference:
         )
 
     def _compute_index_pairs(self):
+        """Return the list of all index pairs for (theta_i, x_target_j)."""
         n_lowest = self.n_lowest
         n_theta, n_x_target = self.distance_precomputed.shape
         if n_lowest == None:
@@ -164,13 +190,13 @@ class GBInference:
         plt.plot(val_losses[:, 0], val_losses[:, 1], "ro-", alpha=0.8)
 
     def _idx_to_data(self, idx_batch):
-        # Load up the thetas, xs, and distances
+        """Look up for theta, x_target, and distance given indices."""
+        # Load up the thetas, xs, and distances.
         theta_batch, x_batch = (
             self.theta[idx_batch[0][:, 0]],
             self.x_target[idx_batch[0][:, 1]],
         )
-
-        # Look up from precomputed distance matrix
+        # Look up from precomputed distance matrix.
         dist_batch = self.distance_precomputed[idx_batch[0][:, 0], idx_batch[0][:, 1]]
 
         return theta_batch, x_batch, dist_batch
@@ -183,7 +209,8 @@ class DistanceEstimator(nn.Module):
         x_dim,
         num_layers,
         hidden_features,
-        net_type="MLP",
+        net_type,
+        positive_constraint_fn=None,
         dropout_prob=0.0,
         use_batch_norm=False,
         activation="relu",
@@ -194,7 +221,7 @@ class DistanceEstimator(nn.Module):
         input_dim = theta_dim + x_dim
         output_dim = 1
         if net_type == "MLP":
-            self.net = nets.MLP(
+            net = nets.MLP(
                 in_shape=[input_dim],
                 out_shape=[output_dim],
                 hidden_sizes=[hidden_features] * num_layers,
@@ -202,21 +229,33 @@ class DistanceEstimator(nn.Module):
             )
 
         elif net_type == "resnet":
-            self.net = nets.ResidualNet(
+            net = nets.ResidualNet(
                 in_features=input_dim,
                 out_features=output_dim,
                 hidden_features=hidden_features,
                 num_blocks=num_layers,
                 dropout_probability=dropout_prob,
                 use_batch_norm=use_batch_norm,
-            )
-        ### TO DO: add activation at the end to force positive distance
+            )    
+        # ### TO DO: add activation at the end to force positive distance
+        # if positive_constraint_fn=='relu':
+        #     net = nn.Sequential(net, nn.ReLU())
+        # elif positive_constraint_fn=='exponential':
+        #     raise NotImplementedError()
+        # else:
+        #     raise NotImplementedError()
+
+        self.net = net
+            
 
     def forward(self, theta, x):
         """
-        Predicts distance between theta and x
+        Predicts distance between theta and x.
         """
         return self.net(torch.concat((theta, x), dim=-1))
+        # return self.net(torch.concat((theta, x), dim=-1)).exp()
+        # return nn.functional.softplus(self.net(torch.concat((theta, x), dim=-1)))
+
 
 
 class GBIPotential(BasePotential):
