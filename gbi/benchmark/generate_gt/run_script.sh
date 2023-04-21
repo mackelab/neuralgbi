dba9ddd379a85a5754acada6e0729e9bbc55bbb6
Richard Gao-slurm <richard.gao@uni-tuebingen.de>
2023-04-21 18:26:24 +0200

    updated all xos on slurm

--- gbi/benchmark/generate_gt/run_script.sh
+++ gbi/benchmark/generate_gt/run_script.sh
@@ -14,3 +14,15 @@
 # python run_two_moons.py --multirun hydra/launcher=my_submitit_slurm task.xo_index=0,1,2,3,4,5,6,7,8,9 task.is_specified='specified','misspecified' task.is_known='known','unknown' task.beta=10,100,1000 task.name=two_moons
 
 # # hydra/launcher=submitit_slurm hydra.launcher.timeout_min=300
+
+
+## generate xos
+python generate_xo.py 'uniform_1d' 10
+python generate_xo.py 'two_moons' 10
+python generate_xo.py 'linear_gaussian' 10
+python generate_xo.py 'gaussian_mixture' 10
+
+# python run_uniform_1d.py --multirun hydra/launcher=my_submitit_slurm task.xo_index=0,1,2,3,4,5,6,7,8,9 task.is_specified='specified','misspecified' task.is_known='known','unknown' task.beta=4,20,100 task.name=uniform_1d
+# python run_two_moons.py --multirun hydra/launcher=my_submitit_slurm task.xo_index=0,1,2,3,4,5,6,7,8,9 task.is_specified='specified','misspecified' task.is_known='known','unknown' task.beta=10,100,1000 task.name=two_moons
+# python run_linear_gaussian.py --multirun hydra/launcher=my_submitit_slurm task.xo_index=0,1,2,3,4,5,6,7,8,9 task.is_specified='specified','misspecified' task.is_known='known','unknown' task.beta=1,10,100 task.name=linear_gaussian
+# python run_gaussian_mixture.py --multirun hydra/launcher=my_submitit_slurm task.xo_index=0,1,2,3,4,5,6,7,8,9 task.is_specified='specified','misspecified' task.is_known='known','unknown' task.beta=10,100,1000 task.name=gaussian_mixture
