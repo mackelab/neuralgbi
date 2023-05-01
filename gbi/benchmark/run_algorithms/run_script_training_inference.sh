8121468f0cf68e85463a128a5f0141ea0ff09ac5
rdgao-lajolla <r.dg.gao@gmail.com>
2023-05-01 23:18:51 +0200

    re-running gaussian_mixture

--- gbi/benchmark/run_algorithms/run_script_training_inference.sh
+++ gbi/benchmark/run_algorithms/run_script_training_inference.sh
@@ -36,9 +36,9 @@ python run_inference.py --multirun hydra/launcher=my_submitit_slurm algorithm=eG
 
 
 # TASK='gaussian_mixture'
-# BETA1=2
-# BETA2=10
-# BETA3=50
+# BETA1=10
+# BETA2=50
+# BETA3=100
 # python run_inference.py --multirun hydra/launcher=my_submitit_slurm algorithm=NPE,NLE trained_inference_datetime=$INF_DATETIME task.name=$TASK task.xo_index=0,1,2,3,4,5,6,7,8,9 task.is_specified=specified,misspecified task.is_known=known,unknown task.beta=1
 # python run_inference.py --multirun hydra/launcher=my_submitit_slurm algorithm=GBI,eGBI,ABC trained_inference_datetime=$INF_DATETIME task.name=$TASK task.xo_index=0,1,2,3,4,5,6,7,8,9 task.is_specified=specified,misspecified task.is_known=known,unknown task.beta=$BETA1,$BETA2,$BETA3
 
