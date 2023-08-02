972ce6739a0a4f994b865be5b189e24bd4734f93
rdgao-lajolla <r.dg.gao@gmail.com>
2023-08-02 19:47:19 +0200

    added shell run script for tempered NLE posterior inference

---
+++ gbi/benchmark/run_algorithms/run_script_inference_tNLE.sh
@@ -0,0 +1,36 @@
+# ## TRAINING MODELS
+# python run_training.py --multirun hydra/launcher=my_submitit_slurm task.name=uniform_1d,two_moons,linear_gaussian,gaussian_mixture algorithm=NPE,NLE,GBI,eGBI,ABC task.num_simulations=200
+
+## do inference
+TASK='uniform_1d'
+INF_DATETIME=`ls ../../../results/benchmark/algorithms/$TASK/ -t | head -n1`
+echo $INF_DATETIME
+# python run_inference.py --multirun hydra/launcher=my_submitit_slurm algorithm=NPE,NLE trained_inference_datetime=$INF_DATETIME task.name=$TASK task.beta=1
+# python run_inference.py --multirun hydra/launcher=my_submitit_slurm algorithm=eGBI,GBI,ABC trained_inference_datetime=$INF_DATETIME task.name=$TASK task.beta=4,20,100
+python run_inference.py --multirun hydra/launcher=my_submitit_slurm algorithm=NLE_tempered trained_inference_datetime=$INF_DATETIME task.name=$TASK task.beta=4,20,100
+
+
+TASK='two_moons'
+INF_DATETIME=`ls ../../../results/benchmark/algorithms/$TASK/ -t | head -n1`
+echo $INF_DATETIME
+# python run_inference.py --multirun hydra/launcher=my_submitit_slurm algorithm=NPE,NLE trained_inference_datetime=$INF_DATETIME task.name=$TASK task.beta=1
+# python run_inference.py --multirun hydra/launcher=my_submitit_slurm algorithm=eGBI,GBI,ABC trained_inference_datetime=$INF_DATETIME task.name=$TASK task.beta=10,100,1000
+python run_inference.py --multirun hydra/launcher=my_submitit_slurm algorithm=NLE_tempered trained_inference_datetime=$INF_DATETIME task.name=$TASK task.beta=10,100,1000
+
+
+TASK='linear_gaussian'
+INF_DATETIME=`ls ../../../results/benchmark/algorithms/$TASK/ -t | head -n1`
+echo $INF_DATETIME
+# python run_inference.py --multirun hydra/launcher=my_submitit_slurm algorithm=NPE,NLE trained_inference_datetime=$INF_DATETIME task.name=$TASK task.beta=1
+# python run_inference.py --multirun hydra/launcher=my_submitit_slurm algorithm=eGBI,GBI,ABC trained_inference_datetime=$INF_DATETIME task.name=$TASK task.beta=1,10,100
+python run_inference.py --multirun hydra/launcher=my_submitit_slurm algorithm=NLE_tempered trained_inference_datetime=$INF_DATETIME task.name=$TASK task.beta=1,10,100
+
+
+TASK='gaussian_mixture'
+INF_DATETIME=`ls ../../../results/benchmark/algorithms/$TASK/ -t | head -n1`
+# INF_DATETIME='2023_05_07__20_08_28' #'2023_05_03__18_41_31'
+echo $INF_DATETIME
+# python run_inference.py --multirun hydra/launcher=my_submitit_slurm algorithm=NPE,NLE trained_inference_datetime=$INF_DATETIME task.name=$TASK task.beta=1
+# python run_inference.py --multirun hydra/launcher=my_submitit_slurm algorithm=eGBI,GBI,ABC trained_inference_datetime=$INF_DATETIME task.name=$TASK task.beta=10,25,75
+python run_inference.py --multirun hydra/launcher=my_submitit_slurm algorithm=NLE_tempered trained_inference_datetime=$INF_DATETIME task.name=$TASK task.beta=10,25,75
+
